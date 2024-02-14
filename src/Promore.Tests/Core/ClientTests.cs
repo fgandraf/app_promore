f998f18a05df7e41dd5bb1ef0d65099e172cc7e6
fgandraf <fgandraf@gmail.com>
2024-02-14 13:45:17 -0300

    feat: Add Client UseCases Tests

---
+++ src/Promore.Tests/Core/ClientTests.cs
@@ -0,0 +1,126 @@
+using Promore.Infra.Repositories.Mock;
+using UseCases = Promore.Core.Contexts.ClientContext.UseCases;
+
+namespace Promore.Tests.Core;
+
+[TestClass]
+public class ClientTests
+{
+    private MockContext context;
+    private ClientRepositoryMock clientRepository;
+    private LotRepositoryMock lotRepository;
+
+    private const string NotExistentCpf = "98765432100";
+    private const int ExistentClientId = 5;
+    
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        context = new MockContext();
+        clientRepository = new ClientRepositoryMock(context);
+        lotRepository = new LotRepositoryMock(context);
+    }
+    
+    [TestMethod]
+    [TestCategory("Create")]
+    public void Create_Client_ReturnsTrue()
+    {
+        var model = BuildCreateClientRequest(NotExistentCpf, existentLotId:"A10");
+        
+        var clientNotExists = !context.Clients.Exists(x => x.Cpf == model.Cpf);
+        var result = new UseCases.Create.Handler(clientRepository,lotRepository).Handle(model).Result;
+        var created = context.Clients.Exists(x => x.Id == result.Value);
+        
+        Assert.IsTrue(clientNotExists && result.Success && created);
+    }
+    
+
+    [TestMethod]
+    [TestCategory("Delete")]
+    public void Delete_Client_ReturnsTrue()
+    {
+        var clientExists = context.Clients.Exists(x => x.Id == ExistentClientId);
+        var result = new UseCases.Delete.Handler(clientRepository).Handle(ExistentClientId).Result;
+        var deleted = !context.Clients.Exists(x => x.Id == ExistentClientId);
+        
+        Assert.IsTrue(clientExists && result.Success && deleted);
+    }
+    
+    
+    [TestMethod]
+    [TestCategory("Get")]
+    public void GetAll_Clients_SameCountThanMock()
+    {
+        const int existentClients = 5;
+        
+        var result = new UseCases.GetAll.Handler(clientRepository).Handle().Result.Value;
+        
+        Assert.AreEqual(existentClients, result.Count);
+    }
+    
+    
+    [TestMethod]
+    [TestCategory("Get")]
+    public void GetAll_ClientsByLotId_SameCountThanMock()
+    {
+        const string existentLotId = "A10";
+        const int existentClientsInLotA10 = 2;
+        
+        var result = new UseCases.GetAllByLotId.Handler(clientRepository).Handle(existentLotId).Result.Value;
+        
+        Assert.AreEqual(existentClientsInLotA10, result.Count);
+    }
+
+    
+    [TestMethod]
+    [TestCategory("Get")]
+    public void Get_ClientByLotId_ReturnsNotNull()
+    {
+        var result = new UseCases.GetById.Handler(clientRepository).Handle(ExistentClientId).Result.Value;
+        
+        Assert.IsNotNull(result);
+    }
+    
+    
+    [TestMethod]
+    [TestCategory("Update")]
+    public void Update_Client_ReturnsTrue()
+    {
+        var existentClientCpf = context.Clients.FirstOrDefault(x => x.Id == ExistentClientId)!.Cpf;
+        var model = BuildUpdateClientRequest(ExistentClientId, NotExistentCpf);
+        
+        var result = new UseCases.Update.UpdateHandler(clientRepository, lotRepository).Handle(model).Result;
+        var updatedClientCpf = context.Clients.FirstOrDefault(x => x.Id == ExistentClientId)!.Cpf;
+        
+        Assert.IsTrue(result.Success && updatedClientCpf != existentClientCpf);
+    }
+    
+
+    private static UseCases.Create.CreateClientRequest BuildCreateClientRequest(string notExistentCpf, string existentLotId)
+    {
+        return new UseCases.Create.CreateClientRequest
+        {
+            Name = "Felipe Gandra",
+            Cpf = notExistentCpf,
+            Phone = "14998290103",
+            MothersName = "Maria Gandra",
+            BirthdayDate = DateTime.Parse("1984-03-01"),
+            LotId = existentLotId
+        };
+    }
+    
+    private static UseCases.Update.UpdateClientRequest BuildUpdateClientRequest(int id, string notExistentCpf)
+    {
+        return new UseCases.Update.UpdateClientRequest
+        {
+            Id = id,
+            Name = "Sonia Contijo Tavares Mock",
+            Cpf = notExistentCpf,
+            Phone = "14986644444",
+            MothersName = "Rita Amália de Jesus",
+            BirthdayDate = new DateTime(1973, 09, 25),
+            LotId = "A10"
+        };
+    }
+}
